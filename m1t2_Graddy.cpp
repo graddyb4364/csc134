02a8363d74f28df720ca01a1453cb57484344a0e
graddyb4364 <157392650+graddyb4364@users.noreply.github.com>
2024-01-22 16:16:34 +0000

    completed m1t2

---
+++ m1t2_Graddy.cpp
@@ -0,0 +1,26 @@
+// CSC 134
+// M1t2
+// Bridgit Graddy
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Introduce yourself
+    cout << "Bridgit Graddy" << endl;
+    cout << "I am enrolled in the Associate of Science program" << endl;
+    cout << "My intrest include space, technoglogy, anime, and video games." << endl;
+    cout << endl;
+
+    // Ask a question
+    string name;
+    cout << "What's your name? ";
+    cin >> name;
+    cout << "Nice to meet you, " << name << "!" << endl;
+    cout << endl << endl;
+
+
+    return 0;
+}
