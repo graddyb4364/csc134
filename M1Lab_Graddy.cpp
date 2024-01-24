772dcc8a8ff20751aff15b4e38f28587ce177659
graddyb4364 <157392650+graddyb4364@users.noreply.github.com>
2024-01-24 16:06:08 +0000

    done M1LAB

---
+++ M1Lab_Graddy.cpp
@@ -0,0 +1,30 @@
+// CSC 134
+// M1Lab - Apple Sales
+// Bridgit Graddy
+// 1/24/2024
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "M1LAB - Apple Sales" << endl;
+    // Simulate selling apples
+    // Set up variables (nouns)
+    string name = "Bridgit";
+    int num_apples = 21; 
+    double price_per_apple = 0.25;
+
+    // Do the calcutlations
+    double total_price = 0;
+    // NOTE: Math operations a + - * /
+    total_price = num_apples * price_per_apple;
+
+    // Print the output  
+    cout << "Welcome to " << name << "'s Apple Orchard!" << endl;
+    cout << "We have " << num_apples << " Apples in stock";
+    cout << " at a cost of $" << price_per_apple << " each." << endl;
+    cout << "For a total cost of $" << total_price << "." << endl;
+
+    return 0;
+}
