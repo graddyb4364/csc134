12f3e4e5ff351c1d5926f09294239195c7587803
graddyb4364 <157392650+graddyb4364@users.noreply.github.com>
2024-01-24 16:50:04 +0000

    done m1hw1

---
+++ M1HW1_Graddy.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M1HW1
+// Bridgit Graddy
+// 1/24/2024
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int year_released = 1994;
+    double gross_sales = 1.66;
+    string movie_name = "The Lion King";
+
+    cout << "The movie " << movie_name << " came out in " << year_released << endl;
+    cout << "and made $" << gross_sales << " billion worldwide at the box office." << endl;
+    cout << endl;
+
+    // Movie summay
+    cout << "The Disney movie is about a young lion named \"Simba\"" << endl;
+    cout << "Simba escapes his homeland as his father \"Mufasa\" is killed" << endl;
+    cout << "The story follows Simba on his jounrey to take back his homeland from his fathers killer." << endl;
+    cout << endl;
+
+    // Favorite movie lines
+    cout << "Infamous lines from the movie include" << endl;
+    cout << "\"Remember who you are\"-Mufasa" << endl; 
+    cout << "\"I laugh in the face of danger.\"-Simba" << endl;
+    cout << "\"Being brave doesn't mean you go looking for trouble\" -Mufasa" << endl;
+    cout << endl;
+
+
+    return 0;
+}
