1a6d6181aa81eccd81cfd9d9bf4e1b618ef99565
graddyb4364 <157392650+graddyb4364@users.noreply.github.com>
2024-01-29 16:35:27 +0000

    finished

--- M2/M2T1_Graddy.cpp
+++ M2/M2T1_Graddy.cpp
@@ -4,6 +4,7 @@
 // 1-29-2024
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
@@ -15,20 +16,25 @@ int main()
    string meal = "Burger Meal";
    double meal_price = 5.99;
    double tax_percent = 0.08;
+   double tax_amount;
+   double total;
 
    // Do the calcutlations
-   double tax_amount = meal_price * tax_percent;
-   double total = meal_price + tax_amount;
+   tax_amount = meal_price * tax_percent;
+   total = meal_price + tax_amount;
 
 
    // Print the receipt
-   //TODO make it line up and fix decimals
+    cout << fixed << setprecision(2);
+
+    cout << "Receipt - M2T1" << endl;
+    cout << "Thank you for coming!" << endl;
+    cout << "-----------------------" << endl;
     cout << meal << "\t $" << meal_price << endl;
-    cout << endl;
     cout << "Tax:" << "\t \t $" << tax_amount << endl;
+    cout << "-----------------------" << endl;
     cout << "Total:" << "\t \t $" << total << endl;
-
-    cout << "Thank you" << endl;
-
+    cout << endl;
+    cout << endl << endl;
     return 0;
 }
