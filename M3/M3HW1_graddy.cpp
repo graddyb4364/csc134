8aaadfd520099526a1a94e844a2ee0254c730f89
graddyb4364 <157392650+graddyb4364@users.noreply.github.com>
2024-02-28 16:12:54 +0000

    2..

---
+++ M3/M3HW1_graddy.cpp
@@ -0,0 +1,35 @@
+//CSC 134
+// M3HW1-tier
+// Bridgit Graddy
+//02/26/2024
+
+#include<iostream>
+using namespace std;
+int main() 
+{
+// Question 1
+string anwser;
+cout << "Hello, I am a chat bot!" << endl;
+cout << "Do you want to chat? Please type yes or no.." << endl;
+cin >> anwser;
+if (anwser == "yes"){
+    cout << "That's great! Let's be friends.";
+}
+else if (anwser == "no"){
+    cout << "Okay..";
+}
+else {
+    cout << "Invaild Input";
+}
+cout << "\n\n";
+// Question 2
+
+
+
+
+
+
+
+
+return 0;
+}
